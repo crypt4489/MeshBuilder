24836a1d1256573fd87922be3cb15fc84d86ebfb
Drew Fletcher <crypt4489@gmail.com>
2023-08-27 01:47:06 -0400

    WIP

--- src/Face.java
+++ src/Face.java
@@ -2,43 +2,47 @@ class Face
 {
    //public int i1, i2, i3;
    public int[] indices = new int[3];
-   
+   public Edge[] edges = new Edge[3];
    public Face()
    {
       this.indices[0] = -1;
       this.indices[1] = -1;
       this.indices[2] = -1;
+      this.edges[0] = this.edges[1] = this.edges[2] = null;
    }
-      
+
    public Face(int index1, int index2, int index3)
    {
       this.indices[0] = index1;
       this.indices[1] = index2;
       this.indices[2] = index3;
+      this.edges[0] = new Edge(index1, index2);
+      this.edges[1] = new Edge(index2, index3);
+      this.edges[2] = new Edge(index3, index1);
    }
-   
+
    public int GetOppositeIndex(Edge e)
    {
       for (int i = 0; i<3; i++)
       {
          int Index = this.indices[i];
-         
+
          if (Index != e.v1 && Index != e.v2)
          {
             return Index;
-         } 
+         }
       }
-      
+
       System.out.println("Something is wrong with opposite face");
       System.exit(0);
-      
+
       return 0;
    }
-   
+
    public void DumpFace()
    {
       System.out.printf("%d %d %d\n", this.indices[0], this.indices[1],this.indices[2]);
    }
-   
-   
+
+
 }
--- src/TriStrip.java
+++ src/TriStrip.java
@@ -1,6 +1,59 @@
+
+import java.util.ArrayList;
+
 class TriStrip
 {
+    class AdjacentTriangle
+    {
+        Face face;
+        Face[] adjFaces = new Face[3];
+
+        public AdjacentTriangle(Face _f)
+        {
+            this.face = _f;
+            for (Face face : adjFaces) {
+                face = null;
+            }
+        }
+    }
+
+    public ArrayList<Integer> CreateAdjacencyTris(ArrayList<Face> faces)
+    {
+
+        return null;
+    }
+
     public static void main(String[] args)   {
+        int[] box = {
+            0, 1, 2,
+            3, 2, 1,
+            6, 5, 4,
+            5, 6, 7,
+            10, 9, 8,
+            9, 10, 11,
+            12, 13, 14,
+            15, 14, 13,
+            16, 17, 18,
+            19, 18, 17,
+            22, 21, 20,
+            21, 22, 23
+        };
+
+        if (box.length % 3 != 0)
+        {
+            System.err.println("Expected Triangles got a number not a multiple of three!!");
+            return;
+        }
+
+        ArrayList<Face> triangles = new ArrayList<Face>();
+
+        for (int i = 0; i<box.length; i+=3)
+        {
+            Face tri = new Face(box[i], box[i+1], box[i+2]);
+            triangles.add(tri);
+        }
+
+
         System.out.println("TriStrips");
     }
 }
